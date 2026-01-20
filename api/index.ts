2681eb091e1b2e0f5bb2a06910a92836cb7342ed
Yasith867 <yasithravindu8678@gmail.com>
2026-01-20 22:04:37 +1100

    Fix Vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,19 @@
-import app from "../dist/index.cjs";
+import express from "express";
+import { registerRoutes } from "../server/routes";
+import { serveStatic } from "../server/static";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+(async () => {
+  await registerRoutes(app);
+
+  if (process.env.NODE_ENV === "production") {
+    serveStatic(app);
+  }
+
+})();
+
 export default app;
